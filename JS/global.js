1be75343f3f89bf309447c2cfd5902ec35a0f42b
Lance Joseph Arceo <144506045+Glass-sama@users.noreply.github.com>
2023-12-17 11:03:24 +0800

    Add files via upload

---
+++ JS/global.js
@@ -0,0 +1,43 @@
+let modeRelax = false;
+let movement = 0;
+let stopwatch;
+let cardGroup = [
+    ["🐚", "🦐"],
+    ["🦞", "🦀"],
+    ["🪼", "🪸","🦑","🐙"],
+    ["🐬","🐳","🐡","🦭"],
+    ["🐢","🐠","🌊","🦈"]
+];
+
+let levelActual = 0;
+let levels = [
+    {
+        cards: cardGroup[0],
+        movementMax: 4
+    },
+    {
+        cards: cardGroup[0].concat(cardGroup[1]),
+        movementMax: 10
+    },
+    {
+        cards: cardGroup[0].concat(
+            cardGroup[1],
+            cardGroup[2]),
+        movementMax: 14
+    },
+    {
+        cards: cardGroup[0].concat(
+            cardGroup[1],
+            cardGroup[2],
+            cardGroup[3]),
+            movementMax: 25
+    },
+    {
+        cards: cardGroup[0].concat(
+            cardGroup[1],
+            cardGroup[2],
+            cardGroup[3],
+            cardGroup[4]),
+            movementMax: 60
+    }
+];
